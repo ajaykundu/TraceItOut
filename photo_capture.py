4b8f128420566ce53d24c81534a95fd19fe50b62
Ajay Kundu <ajaykunduktl2@gmail.com>
2018-04-20 21:23:57 +0530

    total code completed

--- photo_capture.py
+++ photo_capture.py
@@ -9,30 +9,39 @@ import numpy as np
 import cv2
 import time
 
+
 starttime = time.time()
-
 cap = cv2.VideoCapture(0) # video capture source camera (Here webcam of laptop) 
- # return a single frame in variable `frame`
 
+# i is an integer to give numbering to captured image.
 i=0;
 
-# Define the codec and create VideoWriter object
 
 while(cap.isOpened()):
+    # Read frame to capture an image.
     ret, frame = cap.read() 
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
     
-    cv2.imwrite('C:/Users/Dell/Documents/GitHub/capstoneproject/images/photo'+str(i)+'.png',frame)
+    # To show an image in window.
+    cv2.imshow('frame',frame)
+    
+    # location where you want to store your images.
+    location = 'C:/Users/Dell/Documents/GitHub/capstoneproject/images/photo'+str(i)+'.png'
+    
+    # write frame on specified location.
+    cv2.imwrite(location,frame)
+    
+    # var going to store after what time your while loop should stop.
     var = time.time() - starttime  
-    #print(var)
     if ((var) > 20):
         break
-    i = i+1
     
-    #you can change the sleeping time if you want.
+    # if you want to quit press q.
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break 
+    #you can change the sleeping time if you want. 0.005 sec which is currently set.
     time.sleep(0.005)
+    i = i+1  
+    
 # Release everything if job is finished
 cap.release()
 cv2.destroyAllWindows()
